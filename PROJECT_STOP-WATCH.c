33247312f8d1061308f359f3ff951e4b0ecef113
Mehedi Hasan Poni <106755232+mehediponi201@users.noreply.github.com>
2022-06-02 21:29:17 +0600

    Add files via upload

---
+++ PROJECT_STOP-WATCH.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#define CYCLE 5
+#include<windows.h>
+
+int main()
+{
+    int hour,minute,second;
+    printf("Enter the hour minute second: ");
+    scanf("%d %d %d",&hour,&minute,&second);
+
+    int h=0,m=0,s=0;
+    while(1)
+    {      printf("\n\n###### STOP WATCH ######\n\n\n");
+           printf("%.2d:%.2d:%.2d\n",h,m,s);
+           printf("\n\n########################\n\n\n");
+           if(h == hour && m == minute && s == second)
+           {
+               break;
+           }
+
+           s++;
+           sleep(1);
+           if(s == CYCLE)
+           {
+               m++;
+               s=0;
+           }
+           if(m == CYCLE)
+           {
+               h++;
+               m=0;
+           }
+           system("CLS");
+    }
+
+    return 0;
+}
+
